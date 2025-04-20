8cc96d31fa784861c6c0a315dd793473007a0d0c
Louis Maxwell <louismaxwell@knights.ucf.edu>
2025-04-20 19:04:37 -0400

    updated firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDFP1NNsSENaAT8e8OUoFHbP96FfsigwPk",
-    authDomain: "spaced-repetition-study-logger.firebaseapp.com",
-    projectId: "spaced-repetition-study-logger",
-    storageBucket: "spaced-repetition-study-logger.firebasestorage.app",
-    messagingSenderId: "756320605158",
-    appId: "1:756320605158:web:d9ae12fbc507ace1bdfc27"
-};
+    apiKey: process.env.NEXT_PUBLIC_API_KEY!,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN!,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET!,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID!,
+    appId: process.env.NEXT_PUBLIC_APP_ID!,
+  };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
