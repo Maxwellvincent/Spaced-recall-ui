3e336f4cb4e819eab5c2ffcc07e7cac6f0855e48
Louis Maxwell <louismaxwell@knights.ucf.edu>
2025-04-21 00:36:29 -0400

    refactor set

--- migrate_users.mjs
+++ migrate_users.mjs
@@ -1,15 +1,12 @@
 // migrate_user.mjs
 import admin from "firebase-admin";
 
-
-// Load your service account credentials
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
 };
 
-// Initialize admin app (only once)
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
