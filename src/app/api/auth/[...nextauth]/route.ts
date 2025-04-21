9ba0d6183fe46e33391ad7c9bd4b6fc528077946
Louis Maxwell <louismaxwell@knights.ucf.edu>
2025-04-21 15:46:16 -0400

    still working on removing some errors4

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth";
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 import { cert } from "firebase-admin/app";
+import type { NextAuthOptions } from "next-auth";
 
 const firebaseConfig = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
   credential: cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
@@ -10,18 +12,17 @@ const firebaseConfig = {
   }),
 };
 
-export const authOptions = {
+const handler = NextAuth({
   adapter: FirestoreAdapter(firebaseConfig),
   providers: [],
   callbacks: {
     async session({ session, user }) {
       if (session?.user) {
-        session.user.id = user.id;
+        session.user.uid = user.id;
       }
       return session;
     },
   },
-};
+} as NextAuthOptions);
 
-const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST }; 
